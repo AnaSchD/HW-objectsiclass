d4908c03d256b97eca4da248f60ece762a357029
Scherbakova <nastyasch0806@gmail.com>
2022-09-02 21:34:22 +0300

    коммит с исправлениями

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,41 @@ public class Main {
 
 
         Author author1 = new Author("Viktor", "Frankl");
-        Book book = new Book("Say yes to life", author1, 2012);
+        Book book2 = new Book("Say yes to life", author1, 2012);
 
         Author author2 = new Author("Mona", "Kasten");
         Book book1 = new Book("Save you", author2, 2019);
 
-        System.out.println(book.getPublicationYear());
+        System.out.println(book2.getPublicationYear());
 
-        book.setPublicationYear(2015);
-        System.out.println("book.getPublicationYear() = " + book.getPublicationYear());
+        book2.setPublicationYear(2015);
+        System.out.println("book.getPublicationYear() = " + book2.getPublicationYear());
+
+        Book[] books = new Book[5];
+        books[0] = book2;
+        books[1] = book1;
+        addBook(books, book1);
+        addBook(books, book1);
+        addBook(books, book1);
+
+    }
+
+    public static void addBook(Book[] array, Book book) {
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == null) {
+                array [i] = book;
+                return;
+            }
+            if (array[array.length - 1] != null) {
+                throw new RuntimeException("Массив полный");
+            }
+
+
+        }
 
 
     }
 }
+
+
